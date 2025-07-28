816ac270f63cc58c73611b2d10500ace1454c352
kanna2504 <kannathallipotupureddi@gmail.com>
2025-07-28 20:07:58 +0530

    Create FC1b.java

---
+++ flowcontrol/FC1b.java
@@ -0,0 +1,15 @@
+class Fc1b{
+public static void main(String args[]){
+int x1=6;
+int x2=17;
+int r1=x1%10;
+int r2=x2%10;
+if(r1==r2)
+{
+System.out.println("true");
+}
+else{
+System.out.println("false");
+}
+}
+}
