70e74ee07e29aded970bb9f9555a372173e5aa46
kanna2504 <kannathallipotupureddi@gmail.com>
2025-07-28 20:11:29 +0530

    Create Fc5.java

---
+++ flowcontrol/Fc5.java
@@ -0,0 +1,15 @@
+class Fc5{
+public static void main(String args[]){
+char c='0';
+if((c>='a'&&c<='z')||(c>='A'&&c<='z'))
+{
+System.out.println("Alphabet");
+}
+else if(c>='0'&& c<='9'){
+System.out.println("number");
+}
+else{
+System.out.println("special");
+}
+}
+}
