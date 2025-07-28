72712108e5feda7465665036b569299a5c0dab5b
kanna2504 <kannathallipotupureddi@gmail.com>
2025-07-28 20:09:41 +0530

    Create Fc2.java

---
+++ flowcontrol/Fc2.java
@@ -0,0 +1,11 @@
+class Fc2{
+public static void main(String args[]){
+int x=11;
+if(x%2==0){
+System.out.println("it is a even number");
+}
+else{
+System.out.println("it is a odd number");
+}
+}
+}
