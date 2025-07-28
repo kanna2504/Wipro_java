9cf7786219a0b2f41f24147ad16d8fd418f5555d
kanna2504 <kannathallipotupureddi@gmail.com>
2025-07-28 20:05:10 +0530

    Create Ca3.java

---
+++ language_basics/Ca3.java
@@ -0,0 +1,7 @@
+class Ca3{
+public static void main(String args[]){
+int i1=Integer.parseInt(args[0]);
+int i2=Integer.parseInt(args[1]);
+System.out.println("The sum of "+i1+" and "+i2+" is "+(i1+i2));
+}
+}
